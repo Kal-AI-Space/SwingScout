9bbe0ecf970dc9e63a5bd184fd0e3d07cde77aae
Kal-AI-Space <shoutoutsspace@gmail.com>
2026-06-30 14:36:26 +0930

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SwingScout from './SwingScout'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<SwingScout />)
